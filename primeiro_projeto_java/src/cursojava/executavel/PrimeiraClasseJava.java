38cd075464437a075cc77fcd2228a88604806a5a
Pixells <80124753+Pixells@users.noreply.github.com>
2025-01-13 14:54:30 -0300

    Fazendo operações entre números inteiros

--- primeiro_projeto_java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro_projeto_java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,14 +1,17 @@
 package cursojava.executavel;
 
 public class PrimeiraClasseJava {
-
+	
 	/*Main é um metodo auto executavel em Java*/
 	public static void main(String[] args) {
 		
-		/*Escreve o texto no console*/
-		System.out.println("Hello, World!");
-		System.out.println("Meu primeiro programa em Java");
-		System.out.println("Java é orientado a Objetos");
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
+		
+		System.out.println("Media das notas do aluno: " + (nota1 + nota2 + nota3 + nota4) / 4);
+		
+		
 	}
-	
 }
