347375ad5f50084f67a6900feb0c9d3eaa8a0838
Pixells <80124753+Pixells@users.noreply.github.com>
2025-01-14 18:02:59 -0300

    Estrutura de repetição e parada com Break

--- primeiro_projeto_java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro_projeto_java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,14 +5,14 @@ public class PrimeiraClasseJava {
 	/* Main é um metodo auto executavel em Java */
 	public static void main(String[] args) {
 
-		/* Estrutura de repetição FOR */
+		/* Estrutura de repetição e parada com Break */
 		
 		for (int numero = 0; numero <= 10; numero ++) {
-			System.out.println("Número: " + numero);
-		}
-		
-		for (int numero = 10; numero >= 0; numero --) {
-			System.out.println("Número: " + numero);
+			if (numero == 3) {
+				System.out.println("Achamos o número 3");
+				System.out.println("Estou parando de executar...");
+				break;
+			}
 		}
 		
 	}
