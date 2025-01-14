6d03724dd7721d2b3b7ba27461f3a51a991efe98
Pixells <80124753+Pixells@users.noreply.github.com>
2025-01-14 17:10:01 -0300

    Operações lógicas aninhadas

--- primeiro_projeto_java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro_projeto_java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -6,27 +6,28 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 
 		int nota1 = 90;
-		int nota2 = 60;
-		int nota3 = 50;
-		int nota4 = 10;
+		int nota2 = 10;
+		int nota3 = 90;
+		int nota4 = 90;
 		int media = 0;
 
 		media = (nota1 + nota2 + nota3 + nota4) / 4;
 
-		// Condições lógicas com IF e ELSE
-		/*if (media >= 70) {
-			System.out.println("Aluno aprovado: " + media);
-		} else if (media >= 40 && media <= 69) {
-			System.out.println("Aluno em recuperação: " + media);
-		} else {
-			System.out.println("Aluno reprovado: " + media);
-		}*/
-
-		/* Operadores ternários */
-		String saidaResultado = media >= 70 ? "Aluno Aprovado"
-				: (media >= 40 && media <= 69) ? "Aluno em recuperação" : "Aluno reprovado";
-
-		System.out.println(saidaResultado);
+		/*Operações lógicas aninhadas: São operações dentro de operações*/
+		
+		if (media >= 50) {
+			if (media >= 70) {
+				if (media >= 90) {
+					System.out.println("Aluno está aprovado - Parabéns: " + media);
+				}else {
+					System.out.println("Aluno está aprovado direto: " + media);
+				}
+			}else {
+				System.out.println("Aluno está em recuperação: " + media);
+			}
+		}else {
+			System.out.println("Aluno reprovado direto: " + media);
+		}
 
 	}
 }
