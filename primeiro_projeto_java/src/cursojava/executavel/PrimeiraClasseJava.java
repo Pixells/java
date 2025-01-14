2665993f67bf2dce2b7093bf3f5976a18567242b
Pixells <80124753+Pixells@users.noreply.github.com>
2025-01-14 18:39:01 -0300

    Criando nossa entrada de dados, input

--- primeiro_projeto_java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro_projeto_java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,19 +1,25 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 public class PrimeiraClasseJava {
 
 	/* Main é um metodo auto executavel em Java */
 	public static void main(String[] args) {
 
-		/* Divisões e resto com MOD do Java */
-		/* Módulo: Resto da divisão % */
-
-		double carros = 9;
-		double pessoas = 2;
+		/* Criando nossa entrada de dados */
 		
-		double resto = carros % pessoas; /*MOD abreviação de Módulo*/
+		String carros = JOptionPane.showInputDialog("Informe a quantidade de carros: ");
+		String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas: ");
 		
-		System.out.println("Sobraram exatamente: " + resto + " carros.");
+		double carroNumero = Double.parseDouble(carros);
+		double pessoaNumero = Double.parseDouble(pessoas);
+		
+		int divisao = (int) (carroNumero / pessoaNumero); // 4
+		
+		double resto = carroNumero % pessoaNumero;
+		
+		JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao + " carros para cada e sobrou " + resto + " carros.");
 		
 	}
 }
