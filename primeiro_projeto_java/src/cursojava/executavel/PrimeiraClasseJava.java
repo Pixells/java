e9bf0edf7a7c2010950fdba804392530e11c5429
Pixells <80124753+Pixells@users.noreply.github.com>
2025-01-14 15:18:21 -0300

    Operador ternário simples

--- primeiro_projeto_java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro_projeto_java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,7 +5,7 @@ public class PrimeiraClasseJava {
 	/* Main é um metodo auto executavel em Java */
 	public static void main(String[] args) {
 
-		int nota1 = 50;
+		int nota1 = 90;
 		int nota2 = 60;
 		int nota3 = 50;
 		int nota4 = 10;
@@ -13,13 +13,20 @@ public class PrimeiraClasseJava {
 
 		media = (nota1 + nota2 + nota3 + nota4) / 4;
 
-		if (media >= 70) {
+		// Condições lógicas com IF e ELSE
+		/*if (media >= 70) {
 			System.out.println("Aluno aprovado: " + media);
 		} else if (media >= 40 && media <= 69) {
 			System.out.println("Aluno em recuperação: " + media);
 		} else {
 			System.out.println("Aluno reprovado: " + media);
-		}
+		}*/
+
+		/* Operadores ternários */
+		String saidaResultado = media >= 70 ? "Aluno Aprovado"
+				: (media >= 40 && media <= 69) ? "Aluno em recuperação" : "Aluno reprovado";
+
+		System.out.println(saidaResultado);
 
 	}
 }
