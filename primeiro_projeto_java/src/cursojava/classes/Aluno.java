1dba4b80dcac3dac6d3c9c9a995be7a19fe5c00b
Pixells <80124753+Pixells@users.noreply.github.com>
2025-01-16 18:47:12 -0300

    Instanciação, atributos e referências

--- primeiro_projeto_java/src/cursojava/classes/Aluno.java
+++ primeiro_projeto_java/src/cursojava/classes/Aluno.java
@@ -3,6 +3,7 @@ package cursojava.classes;
 // Está é nossa classe/objeto que representa o Aluno
 public class Aluno {
 	
+	/* Esses são os atributos do Aluno */
 	String nome;
 	int idade;
 	String dataNascimento;
--- primeiro_projeto_java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro_projeto_java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -6,7 +6,9 @@ public class PrimeiraClasseJava {
 
 	/* Main é um metodo auto executavel em Java */
 	public static void main(String[] args) {
-
+	
+		/* new Aluno() é uma instância (Criação de Objeto) */
+		/* aluno1 é uma referência para o objeto aluno */
 		Aluno aluno1 = new Aluno(); /* Aqui será o João */
 
 		Aluno aluno2 = new Aluno(); /* Aqui será o Pedro */
